0f545ab70738123b8fbc2c67a8354b7ddcb03534
Jere Saastamoinen <jere@evermade.fi>
2020-01-24 11:13:25 +0000

    slogan fix

--- app/src/wp-content/themes/swiss/templates/_slogan.php
+++ app/src/wp-content/themes/swiss/templates/_slogan.php
@@ -1,4 +1,4 @@
-<?php if (get_field('opt_primary_tagline', 'option') && get_field('opt_secondary_tagline', 'option')):?>
+<?php if (get_field('opt_primary_tagline', 'option') || get_field('opt_secondary_tagline', 'option')):?>
 <div class="c-slogan">
 <p><?php if (get_field('opt_primary_tagline', 'option')):?><span><?php the_field('opt_primary_tagline', 'option');?></span><?php endif;?><?php the_field('opt_secondary_tagline', 'option');?></p>
 </div>
